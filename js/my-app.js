859966106a88df3429b492362fc8ce312dcc4a73
jtfl2008 <jtfl2008@gmail.com>
2015-04-21 14:17:18 +0800

    add fils

---
+++ js/my-app.js
@@ -0,0 +1,22 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true,
+    pushState: true,
+    swipePanel: 'left'
+});
+
+var myApp = new Framework7(); 
+ 
+  // Init slider and store its instance in mySwiper variable
+  var mySwiper = myApp.swiper('.swiper-container', {
+    pagination:'.swiper-pagination',
+    paginationClickable: true,
+    loop: true
+  }); 
